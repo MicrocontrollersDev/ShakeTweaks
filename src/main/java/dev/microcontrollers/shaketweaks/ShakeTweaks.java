a2188c9b3a801e7e4653d35ed8a7d8e5fe7f5af9
MicrocontrollersDev <microcontrollersyt@gmail.com>
2023-07-20 21:06:21 -0700

    fix config not saving and incorrect logic

--- src/main/java/dev/microcontrollers/shaketweaks/ShakeTweaks.java
+++ src/main/java/dev/microcontrollers/shaketweaks/ShakeTweaks.java
@@ -6,7 +6,7 @@ import net.fabricmc.api.ModInitializer;
 public class ShakeTweaks implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		ShakeTweaksConfig.configInstance.load();
+		ShakeTweaksConfig.INSTANCE.load();
 	}
 
 }
--- src/main/java/dev/microcontrollers/shaketweaks/config/ShakeTweaksConfig.java
+++ src/main/java/dev/microcontrollers/shaketweaks/config/ShakeTweaksConfig.java
@@ -6,64 +6,65 @@ import dev.isxander.yacl3.api.OptionDescription;
 import dev.isxander.yacl3.api.YetAnotherConfigLib;
 import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
 import dev.isxander.yacl3.config.ConfigEntry;
+import dev.isxander.yacl3.config.ConfigInstance;
 import dev.isxander.yacl3.config.GsonConfigInstance;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 
-import java.nio.file.Path;
-
 public class ShakeTweaksConfig {
-    public static GsonConfigInstance<dev.microcontrollers.shaketweaks.config.ShakeTweaksConfig> configInstance = new GsonConfigInstance<>(dev.microcontrollers.shaketweaks.config.ShakeTweaksConfig.class, Path.of(FabricLoader.getInstance().getConfigDir().toString(), "shaketweaks.json"));
+    public static final ConfigInstance<ShakeTweaksConfig> INSTANCE = GsonConfigInstance.createBuilder(ShakeTweaksConfig.class)
+            .setPath(FabricLoader.getInstance().getConfigDir().resolve("shaketweaks.json"))
+            .build();
 
     @ConfigEntry
-    public static boolean disableScreenBobbing;
+    public boolean disableScreenBobbing = true;
 
     @ConfigEntry
-    public static boolean disableHandBobbing;
+    public boolean disableHandBobbing = false;
 
     @ConfigEntry
-    public static boolean disableMapBobbing;
+    public boolean disableMapBobbing = true;
 
     @ConfigEntry
-    public static boolean disableHandDamage;
+    public boolean disableHandDamage = false;
 
     @ConfigEntry
-    public static boolean disableScreenDamage;
+    public boolean disableScreenDamage = false;
 
     public static Screen configScreen(Screen parent) {
-        return YetAnotherConfigLib.create(configInstance, ((defaults, config, builder) -> builder
+        return YetAnotherConfigLib.create(INSTANCE, ((defaults, config, builder) -> builder
                 .title(Text.literal("Better View Bobbing"))
                 .category(ConfigCategory.createBuilder()
                         .name(Text.literal("Better View Bobbing"))
                         .option(Option.createBuilder(boolean.class)
                                 .name(Text.literal("Disable Screen Bobbing"))
                                 .description(OptionDescription.of(Text.of("Disables the screen shake when moving.")))
-                                .binding(false, () -> disableScreenBobbing, newVal -> disableScreenBobbing = newVal)
+                                .binding(defaults.disableScreenBobbing, () -> config.disableScreenBobbing, newVal -> config.disableScreenBobbing = newVal)
                                 .controller(TickBoxControllerBuilder::create)
                                 .build())
                         .option(Option.createBuilder(boolean.class)
                                 .name(Text.literal("Disable Hand Bobbing"))
                                 .description(OptionDescription.of(Text.of("Disables the hand shake when moving.")))
-                                .binding(false, () -> disableHandBobbing, newVal -> disableHandBobbing = newVal)
+                                .binding(defaults.disableHandBobbing, () -> config.disableHandBobbing, newVal -> config.disableHandBobbing = newVal)
                                 .controller(TickBoxControllerBuilder::create)
                                 .build())
                         .option(Option.createBuilder(boolean.class)
                                 .name(Text.literal("Disable Map Bobbing"))
-                                .description(OptionDescription.of(Text.of("Disables the hand shake when holding a map.")))
-                                .binding(false, () -> disableMapBobbing, newVal -> disableMapBobbing = newVal)
+                                .description(OptionDescription.of(Text.of("Disables the hand shake when holding a map. Does nothing if \"Disable Hand Bobbing\" is turned on.")))
+                                .binding(defaults.disableMapBobbing, () -> config.disableMapBobbing, newVal -> config.disableMapBobbing = newVal)
                                 .controller(TickBoxControllerBuilder::create)
                                 .build())
                         .option(Option.createBuilder(boolean.class)
                                 .name(Text.literal("Disable Screen Damage Tilt"))
                                 .description(OptionDescription.of(Text.of("Disables the screen shake when taking damage.")))
-                                .binding(false, () -> disableScreenDamage, newVal -> disableScreenDamage = newVal)
+                                .binding(defaults.disableScreenDamage, () -> config.disableScreenDamage, newVal -> config.disableScreenDamage = newVal)
                                 .controller(TickBoxControllerBuilder::create)
                                 .build())
                         .option(Option.createBuilder(boolean.class)
                                 .name(Text.literal("Disable Hand Damage Tilt"))
                                 .description(OptionDescription.of(Text.of("Disables the hand shake when taking damage.")))
-                                .binding(false, () -> disableHandDamage, newVal -> disableHandDamage = newVal)
+                                .binding(defaults.disableHandDamage, () -> config.disableHandDamage, newVal -> config.disableHandDamage = newVal)
                                 .controller(TickBoxControllerBuilder::create)
                                 .build())
                         .build())
